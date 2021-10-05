a86db78b5680bf5389c51b3b200ec4a0b1891cb8
David <david@erarnitox.de>
2021-10-05 03:38:19 +0200

    created main

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+#include <Player.h>
+
+int main(){
+   //create our player:
+   Player player;
+
+   //display the welcome message:
+   puts("Welcome to ROTTEN!\nThis game was developed by Erarnitox.");
+
+   //game loop:
+   char* input[100];
+   while(strncmp(input, "quit", 100)){
+   	//game logic
+   }
+   
+}
